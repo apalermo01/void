1f45198cd4305df709941e6f1b7cd661d9b292b7
WTWB-none <htkbolas@mail.ru>
2025-02-23 19:42:05 +0300

    added background gradient

--- src/lib/components/WelcomeAnim.svelte
+++ src/lib/components/WelcomeAnim.svelte
@@ -21,15 +21,15 @@
 		<button
 			id="left"
 			aria-label="prev"
-			onclick={() => (current_info == 0 ? current_info : current_info--)}
+			onclick={() => (current_info === 0 ? current_info : current_info--)}
 		></button>
-		{#if current_info == welcome_info.length - 1}
+		{#if current_info === welcome_info.length - 1}
 			<button id="lessgo" onclick={lessgo}>Хочу попробовать!</button>
 		{/if}
 		<button
 			id="right"
 			aria-label="next"
-			onclick={() => (current_info == welcome_info.length - 1 ? current_info : current_info++)}
+			onclick={() => (current_info === welcome_info.length - 1 ? current_info : current_info++)}
 		></button>
 	</div>
 </div>
@@ -43,6 +43,14 @@
 		min-height: 100vh;
 		width: 80%;
 		margin: 0 auto;
+		background: radial-gradient(
+			circle at center,
+			rgba(255, 255, 255, 0.1) 0%,
+			rgba(22, 24, 31, 0.8) 50%,
+			#16181f 70%
+		);
+		/* Добавляем базовый цвет фона для плавного перехода */
+		background-color: #16181f;
 	}
 	h1 {
 		color: white;
@@ -54,22 +62,37 @@
 		display: flex;
 		justify-content: center;
 		align-items: center;
+		background-color: transparent;
 	}
 
 	button {
 		min-width: 50px;
 		min-height: 50px;
-		background-color: #16181f;
+		background-color: transparent;
 		color: white;
 		cursor: pointer;
 		border: none;
+		position: relative; /* Для позиционирования псевдоэлементов */
 	}
 
-	#left {
+	#left::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 		background-image: url('leftarr.svg');
 		background-size: cover;
 	}
-	#right {
+
+	#right::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 		background-image: url('rightarr.svg');
 		background-size: cover;
 	}
