a8643f7adb5ea3138a261e4eefa663673011f31a
Alex <alex.palermo.ai@gmail.com>
2025-07-23 23:40:37 -0400

    Started building utility for verifying that locales are updated

--- src/tests/lib/utils.test.ts
+++ src/tests/lib/utils.test.ts
@@ -1,12 +1,17 @@
 import { expect, test } from 'vitest'
 import { validateLocales } from '@/lib/utils';
 
-// Local comparison checks
-// function testLocaleComparisons(): boolean {
-//     return true;
-// }
 
-test('test locales', async () => {
-    const val = await validateLocales("src/locales")
-    expect(val).toBe(true);
-});
+for (let testCase of [['succ1', true], ['succ2', true], ['fail1', false], ['fail2', false]]) {
+    console.log("test case = ");
+    console.log(testCase);
+    let elem: string = testCase[0];
+    let res: boolean = testCase[1];
+    console.log("elem = " + elem);
+    console.log("res = " + res);
+    test(elem, async () => {
+        const val = await validateLocales(`src/tests/fixtures/locales/${elem}`)
+        expect(val).toBe(res);
+    });
+
+}
