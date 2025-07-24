e4c754f6593652c002b0d35ffcd9ddcc977bd14e
Alex <alex.palermo.ai@gmail.com>
2025-07-23 22:55:25 -0400

    Set up utility function to check that objects have the same keys

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,24 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { readdir, readFile } from 'node:fs/promises';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
+export async function validateLocales(localePath: string): Promise<boolean> {
+    const files = await readdir(localePath)
+    const contents = await Promise.all(
+        files.map((f: string) =>
+            readFile(`${localePath}/${f}`, 'utf8'))
+    )
+    const contents_parsed = contents.map(r => JSON.parse(r))
 
+    return compareKeys(contents_parsed)
+}
+
+function compareKeys(...objs: any[]): boolean {
+    const allKeys = objs.reduce((keys, object) => keys.concat(Object.keys(object)), [])
+    const union = new Set(allKeys);
+    return objs.every(object => union.size === Object.keys(object).length);
+}
