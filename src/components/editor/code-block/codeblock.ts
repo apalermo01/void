c065f547b3e51c9c475cc0e14b33f081c9dfd276
WTWB-none <htkbolas@mail.ru>
2025-07-29 23:22:57 +0300

    fixed markdown not appeared on click

--- src/components/editor/code-block/codeblock.ts
+++ src/components/editor/code-block/codeblock.ts
@@ -15,6 +15,7 @@ class CodeBlockWidget extends WidgetType {
     language.className = 'cm-code-lang';
     copyButton.className = 'cm-code-button';
     copyButton.style.display = 'none';
+    copyButton.style.zIndex = '100';
     copyButton.innerHTML = "<svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'><rect stroke='none' fill='#ffffff' opacity='0'/><g transform=\"matrix(1 0 0 1 12 12)\" ><path style=\"stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(128,128,128); fill-rule: nonzero; opacity: 1;\" transform=\" translate(-12, -12)\" d=\"M 4 2 C 2.895 2 2 2.895 2 4 L 2 18 L 4 18 L 4 4 L 18 4 L 18 2 L 4 2 z M 8 6 C 6.895 6 6 6.895 6 8 L 6 20 C 6 21.105 6.895 22 8 22 L 20 22 C 21.105 22 22 21.105 22 20 L 22 8 C 22 6.895 21.105 6 20 6 L 8 6 z M 8 8 L 20 8 L 20 20 L 8 20 L 8 8 z\" stroke-linecap=\"round\" /></g></svg>"
     copyButton.addEventListener('click', () => {
       writeText(this.code);
@@ -37,6 +38,10 @@ class CodeBlockWidget extends WidgetType {
     }, null);
     el.className = 'cm-code';
     return el;
+  }
+  ignoreEvent(event: Event): boolean {
+    const target = event.target as HTMLElement;
+    return !!target.closest('.cm-code-button');
   }
 }
 
