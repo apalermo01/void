0d3e0d52073e6b11e020aff6a3b6a6877c2c5af4
Alex <alex.palermo.ai@gmail.com>
2025-08-06 05:39:58 -0400

    Got validateLocales utility working
    I originally messed up the function definition for compareKeys. `...objs: any[]` refers to an arbitrary number of arguments, where each argument is a list. Now, it expects a single argument which is a list.
    
    Also cleaned up the test case itself by creating an interface for each case.

--- src/tests/lib/utils.test.ts
+++ src/tests/lib/utils.test.ts
@@ -1,14 +1,21 @@
 import { expect, test } from 'vitest'
 import { validateLocales } from '@/lib/utils';
 
+interface Case {
+    path: string,
+    shouldPass: boolean
+};
 
-for (let testCase of [['succ1', true], ['succ2', true], ['fail1', false], ['fail2', false]]) {
-    console.log("test case = ");
-    console.log(testCase);
-    let elem: string = testCase[0];
-    let res: boolean = testCase[1];
-    console.log("elem = " + elem);
-    console.log("res = " + res);
+let cases: Case[] = [
+    { path: 'succ1', shouldPass: true },
+    { path: 'succ2', shouldPass: true },
+    { path: 'fail1', shouldPass: false },
+    { path: 'fail2', shouldPass: false },
+];
+
+for (let testCase of cases) {
+    let elem: string = testCase.path;
+    let res: boolean = testCase.shouldPass;
     test(elem, async () => {
         const val = await validateLocales(`src/tests/fixtures/locales/${elem}`)
         expect(val).toBe(res);
